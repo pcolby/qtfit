a24fc85dddd1181e3c41dbb5deeda19dd5405a19
Paul Colby <git@colby.id.au>
2021-08-07 08:55:10 +1000

    Add test coverage report

--- .update.sh
+++ .update.sh
@@ -4,6 +4,7 @@ set -o errexit -o noclobber -o nounset -o pipefail
 
 : "${BUILD_DIR:=$HOME/tmp/build/fit-cmake-gcc-6.1.1}"
 
-rm -rf {internal/,}main
+rm -rf {coverage/,internal/,}main
 cp -a "$BUILD_DIR/doc/public" main
 cp -a "$BUILD_DIR/doc/internal" internal/main
+cp -a "$BUILD_DIR/coverage" coverage/main
